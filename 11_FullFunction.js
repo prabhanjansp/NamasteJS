e498917abb3ac2cc0332a374b3571bf938b00455
prabhanjansp <puranikchetan97@gmail.com>
2025-02-11 20:16:29 +0530

    about all the types of the function

---
+++ 11_FullFunction.js
@@ -0,0 +1,17 @@
+/**
+ * function statement
+ * 
+ * function expression
+ * 
+ * function declaration
+ * 
+ * Anonymous functions
+ * 
+ * named function expression
+ * 
+ * difference between parameter and arguments
+ * 
+ * first class function
+ * 
+ * arrow function
+ */
