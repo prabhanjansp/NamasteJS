36befb1ffc58e9e4937dc3d8584c25e20cf98af5
prabhanjansp <puranikchetan97@gmail.com>
2025-02-12 19:07:48 +0530

    find max with reducer

--- 15_ArrayMethods.js
+++ 15_ArrayMethods.js
@@ -19,11 +19,51 @@ function isOdd(x) {
   return x % 2;
 }
 function isEven(x) {
-  return x % 2===0;
+  return x % 2 === 0;
 }
 function greaterThan4(x) {
-  return x>4;
+  return x > 4;
 }
-const res = arr.filter(isOdd);console.log(res)
-const res1 = arr.filter(isEven);console.log(res1)
-const res3 = arr.filter(greaterThan4);console.log(res3)
+const res = arr.filter(isOdd);
+console.log(res);
+const res1 = arr.filter(isEven);
+console.log(res1);
+const res3 = arr.filter(greaterThan4);
+console.log(res3);
+
+// *************************************************************
+// reducer
+function findSum(arr) {
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
+console.log(findSum(arr));
+
+const total = arr.reduce(function (acc, curr) {
+  acc = acc + curr;
+  return acc;
+}, 0);
+
+console.log("reducer", total);
+//************************************************************* */
+function findMax() {
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+console.log(findMax(arr));
+/************************************************************** */
+const res4 = arr.reduce(function (acc, curr) {
+  if (curr > acc) {
+    acc = curr;
+  }
+  return acc;
+}, 0);
+console.log(res4);
