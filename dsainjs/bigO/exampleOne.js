3dc0d1f42317748f6ccad3f9413b83da865ebad3
prabhanjansp <puranikchetan97@gmail.com>
2025-02-17 07:37:09 +0530

    time complexity discussion

--- dsainjs/bigO/exampleOne.js
+++ dsainjs/bigO/exampleOne.js
@@ -14,9 +14,48 @@ console.log(`time taken to  complete the task ${(t2 - t1) / 1000} seconds`);
 /************************************************************************************* */
 // faster apprach
 function addSum(n) {
-  return n * (n + 1) / 2;
+  return (n * (n + 1)) / 2;
 }
 let t11 = performance.now();
 console.log(addSum(1000000000));
 let t22 = performance.now();
 console.log(`time taken to  complete the task ${(t22 - t11) / 1000} seconds`);
+
+/** count up and down */
+function upAndDown(n) {
+  console.log("going up");
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+  console.log("at the top and going down");
+  for (let j = n - 1; j >= 0; j--) {
+    console.log(j);
+  }
+  console.log("back down");
+}
+
+function printallParis(n){
+    for (let i = 0; i < n; i++){
+        for(let j=0;j<=n;j++)
+        {
+            console.log(i,j)
+        }
+    }
+}
+console.log(printallParis(10))
+
+/************************************************ */
+function logAtleast5(n){
+  for(let i=0;i<=Math.max(5,n);i++){
+    console.log(i)
+  }
+}
+console.log(logAtleast5(10))
+
+/************************************************ */
+function logAtleast55(n){
+  for(let i=0;i<=Math.min(5,n);i++){
+    console.log(i)
+  }
+}
+console.log(logAtleast55(10))
